ee6defe8e2f5731afa8e03f14e4b366b83fefbce
2Deathadder <3thomwinner2007@gmail.com>
2026-08-10 21:02:42 +0000

    Ajout/MAJ via génération IA 21st.dev : tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: { extend: { fontFamily: { sans: ['Inter', 'ui-sans-serif', 'system-ui'], display: ['Rajdhani', 'Inter', 'sans-serif'] } } },
+  plugins: []
+}
