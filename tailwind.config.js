72639c7c32e12a0302168005f5eac33715b030f9
2Deathadder <3thomwinner2007@gmail.com>
2026-08-11 01:36:46 +0000

    Mise à jour via génération IA 21st.dev : tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { fontFamily: { sans: ['Sora', 'ui-sans-serif', 'system-ui'] }, colors: { legion: '#ef3340' } } }, plugins: [] }
+export default { content: ['./index.html','./src/**/*.{js,jsx}'], theme: { extend: { fontFamily: { display: ['Sora','sans-serif'], sans: ['Outfit','sans-serif'] } } }, plugins: [] }
