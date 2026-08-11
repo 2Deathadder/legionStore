c2481de4adf6af5af309b589d4ed575168e218d7
2Deathadder <3thomwinner2007@gmail.com>
2026-08-11 01:36:49 +0000

    Mise à jour via génération IA 21st.dev : src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { StoreProvider } from './context/StoreContext'
 import App from './App'
 import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><StoreProvider><App /></StoreProvider></BrowserRouter></React.StrictMode>)
+createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
