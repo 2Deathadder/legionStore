934742872bb027e56fd99108834a08b530e5e2ae
2Deathadder <3thomwinner2007@gmail.com>
2026-08-11 01:36:47 +0000

    Mise à jour via génération IA 21st.dev : src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,11 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
-
-const firebaseConfig = {
-  apiKey: 'À REMPLACER',
-  authDomain: 'À REMPLACER.firebaseapp.com',
-  projectId: 'À REMPLACER',
-  storageBucket: 'À REMPLACER.appspot.com',
-  messagingSenderId: 'À REMPLACER',
-  appId: 'À REMPLACER'
-}
 
 // À REMPLACER - crée un projet gratuit sur https://console.firebase.google.com
+const firebaseConfig = {
+  apiKey: 'REMPLACER', authDomain: 'REMPLACER.firebaseapp.com', projectId: 'REMPLACER',
+  storageBucket: 'REMPLACER.appspot.com', messagingSenderId: 'REMPLACER', appId: 'REMPLACER'
+}
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
-export const auth = getAuth(app)
+export default app
